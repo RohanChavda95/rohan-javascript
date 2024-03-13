4209215244810bce3c50759fea9ae14562e58539
RohanChavda95 <chavdarohan311@gmail.com>
2024-03-13 18:37:53 +0530

    first commit of js

---
+++ first.js
@@ -0,0 +1,40 @@
+// let score = prompt("Enter a marks: ");
+
+// if(score>=90 && score<=100)
+// {
+//     console.log("Grade is A");
+// }
+// else if(score>=70 && score<=89)
+// {
+//     console.log("Grade is B");
+// }
+// else if(score>=60 && score<=69)
+// {
+//     console.log("Grade is C");
+// }
+// else if(score>=50 && score<=59)
+// {
+//     console.log("Grade is D");
+// }
+// else if(score>=0 && score<=49)
+// {
+//     console.log("Grade is F");
+// }
+// else
+// {
+//     console.log("Input is not valid");
+// }
+
+// for (let count=1; count<=100000; count++)
+// {
+//     console.log("hello lawde");
+// }
+// console.log("fuck of");
+
+for (let i=0; i<=100; i++)
+{
+    if(i%2!==0)
+    {
+        console.log(i);
+    }
+}
