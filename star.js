a4b4e64e4a67e6d6dbafc10f0b922e0a93e2b409
RohanChavda95 <chavdarohan311@gmail.com>
2024-03-20 18:09:59 +0530

    star pattern and digit pattern

--- star.js
+++ star.js
@@ -10,12 +10,12 @@
 //   console.log(str.repeat(i));
 // }
 
-let n = 5;
-for (let i = 1; i <= n; i++) {
-  let str = "* ";
-  let space = ' ';
-  console.log(space.repeat((n - i)) + str.repeat(i));
-}
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   let str = "* ";
+//   let space = ' ';
+//   console.log(space.repeat((n - i)) + str.repeat(i));
+// }
 
 // let n = 5;
 // for (let i = n; i >= 1; i--) {
